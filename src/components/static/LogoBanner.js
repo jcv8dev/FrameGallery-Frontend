4ed3baa7fdb26c51ef5b02c2188875f2157ce63c
jcvb <github@jcvb.de>
2024-06-21 14:33:39 +0200

    add artist name to logo banner

--- src/components/static/LogoBanner.js
+++ src/components/static/LogoBanner.js
@@ -1,10 +1,32 @@
 import {Col, Row} from "react-bootstrap";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 const LogoBanner = () => {
+
+    const [artistInfo, setArtistInfo] = useState({})
+
+    const fetchArtistInfo = async () => {
+        let artistInfo_ = {}
+        await axios.get("/api/rest/v1/artist/info").then(res => {
+            setArtistInfo(res.data);
+        }).catch(err => {
+            console.error(err)
+        })
+    }
+
+     useEffect( () => {
+        fetchArtistInfo()
+        console.debug("Rendered Banner")
+    }, []);
+
     return(
         <>
-            <Row className={"py-3"}>
-                <Col><h1 className={"mx-auto"} style={{width: "fit-content"}}>Logo</h1></Col>
+            <Row className={"pt-3 pb-2"}>
+                <Col><h3 className={"mx-auto"} style={{width: "fit-content"}}>Logo</h3></Col>
+            </Row>
+            <Row className={"pt-2 pb-3"}>
+                <Col><h1 className={"mx-auto"} style={{width: "fit-content"}}>{artistInfo.name}</h1></Col>
             </Row>
         </>
     )
