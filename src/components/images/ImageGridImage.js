22c9865e7c22cd337f096f7ea86ccc7953f04b1e
jcvb <github@jcvb.de>
2024-06-26 00:41:37 +0200

    update path logic

--- src/components/images/ImageGridImage.js
+++ src/components/images/ImageGridImage.js
@@ -1,12 +1,34 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 const ImageGridImage = (props) => {
 
     let cursor = props.cursor || "default"
 
+    const [published, setPublished] = useState(false)
+
+    useEffect(() => {
+        getImageInfo()
+    }, []);
+
+    const getImageInfo = () => {
+        axios.get(`/api/rest/v1/image/${props.id}`).then(res => {
+            setPublished(res.data.published)
+        }).catch(err => {
+            console.error(err)
+        })
+    }
+
     return (
-        <div className={"bg-light-subtle p-2 m-1 shadow-sm"}>
-            <img style={{cursor: cursor}} className={"img-fluid"} src={props.src} alt={""} onClick={props.clickHandler}/>
+        <div className={"bg-light-subtle p-2 m-1 shadow-sm"} style={{ position: 'relative' }}>
+            {props.highlightUnpublished && !published ? (
+                <div id={props.id} className="fg-unpublished-cover" onClick={props.clickHandler}>
+                    Not Published
+                </div>
+            ) : (<></>)}
+            <img id={props.id} style={{cursor: cursor}} className={"img-fluid"} src={props.src} alt={""} onClick={props.clickHandler}/>
         </div>
-    )
+    );
 }
 
 export default ImageGridImage
