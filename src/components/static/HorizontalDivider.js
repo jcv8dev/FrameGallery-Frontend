7642f80642f9ea377fb6993489d7fc621cd05636
jcvb <github@jcvb.de>
2024-06-26 00:40:48 +0200

    add divider

---
+++ src/components/static/HorizontalDivider.js
@@ -0,0 +1,10 @@
+const HorizontalDivider = () => {
+
+    return(
+        <>
+            <div className={"bg-light-subtle mb-3 mx-auto"} style={{height: "0.2rem", width: "90%"}}></div>
+        </>
+    )
+}
+
+export default HorizontalDivider
